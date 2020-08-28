27f6c36a1b43bce28a56512a65b912f09b116c6a
Corey Martin <coreyjmartin2@gmail.com>
2020-08-28 14:43:00 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,9 +9,10 @@ def summon_captain_planet(planeteer_calls)
   end
 
 def long_planeteer_calls(words)
-  words.each |i|
+  words.each do |i|
   if i.
-  # Your code here
+  end 
+
 end
 
 def find_the_cheese# code an argument here
